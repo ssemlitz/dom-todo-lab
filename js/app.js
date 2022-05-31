fcd567e088c3a651515f09050305d8e93cb6c0ba
ssemlitz <sdsemlitz@yahoo.com>
2022-05-31 16:33:01 -0400

    complete button event listener

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-console.log('sanity')
+const input = document.getElementById('inp')
+const button = document.getElementById('submit-button')
+const ul = document.getElementById('todo-list')
+
+button.addEventListener('click', addToList)
+
+function addToList() {
+  const li = document.createElement('li')
+  const inp = document.querySelector('input')
+  li.textContent = inp.value 
+  document.querySelector('ul').appendChild(li)
+  inp.value = ''
+}
